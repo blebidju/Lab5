f51f3d860d5b94bdf8a4d222826eea5afa4ffdcb
blebidju <blebidju@asu.edu>
2017-11-15 19:21:23 -0700

    commit 3

--- _lab5_act1.js
+++ _lab5_act1.js
@@ -1,6 +1,5 @@
 
-function createTable(){
-  document.getElementById("Table").innerHTML =  '<table style="width:100%">'+
+var TableString = '<table style="width:100%">'+
 '  <tr>'+
 '    <th>City Name</th>'+
 '    <th>Timestamp<br/>(yyyy:mm:dd:hh:mm:ss)</th> '+
@@ -8,43 +7,70 @@ function createTable(){
 '    <th>Humidity <br/>in %</th>'+
 '    <th>Wind Speed<br/>in miles per hour</th> '+
 '    <th>Cloudiness<br/>in %</th>'+
-'  </tr>'+
-'  <tr>'+
-'    <td>Tokyo,JP</td>'+
-'    <td>Smith</td>'+
-'    <td>50</td>'+
-'    <td></td>'+
-'    <td></td>'+
-'    <td></td>'+
-'  </tr>'+
-'  <tr>'+
-'    <td>Eve</td>'+
-'    <td>Jackson</td>'+
-'    <td>94</td>'+
-'    <td></td>'+
-'    <td></td>'+
-'    <td></td>'+
-'  </tr>'+
-'</table>';
-  
+'  </tr>';
 
+var FirstCityInfo = "";
+
+var SecondCityInfo = "";
+
+var ThirdCityInfo = "";
+
+function grabInfo(){
+  //london city id
+  loadDoc("2643743");
+  //tokyo city id
+  loadDoc("1850147");
 }
 
-function loadDoc() {
+
+function loadDoc(id) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
-      parseRequest(this.responseText);
+      interpretRequest(JSON.parse(this.responseText));
     }
   };
-  xhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=29e67ba9c14f9f26ee408b825f02a00c", true);
+  xhttp.open("GET", "http://api.openweathermap.org/data/2.5/weather?id="+id+"&APPID=29e67ba9c14f9f26ee408b825f02a00c", true);
   xhttp.send();
-  document.getElementById("demo").innerHTML = this.responseText;
-
+  
 }
 
-function parseRequest(request){
-  var parsedText = JSON.parse(request);
-  document.getElementById("demo").innerHTML = parsedText.main.temp;
+
+function interpretRequest(request){
+  if(request.name == "London"){
+    FirstCityInfo = request;
+  }
+  else if(request.name == "Tokyo"){
+    SecondCityInfo = request;
+  }
+  else{
+    document.getElementById("demo").innerHTML = "3";
+  }
+
+  if(FirstCityInfo != "" && SecondCityInfo != ""){
+    document.getElementById("Table").innerHTML = TableString +
+    '  <tr>'+
+    '    <td>'+FirstCityInfo.name+'</td>'+
+    '    <td>'+convertTime(FirstCityInfo.dt)+'</td>'+
+    '    <td>'+(FirstCityInfo.main.temp-273.15)+'</td>'+
+    '    <td>'+FirstCityInfo.name+'</td>'+
+    '    <td>'+FirstCityInfo.name+'</td>'+
+    '  </tr>'+
+    '  <tr>'+
+    '    <td>'+SecondCityInfo.name+'</td>'+
+    '    <td>'+SecondCityInfo.name+'</td>'+
+    '    <td>'+(SecondCityInfo.main.temp-273.15)+'</td>'+
+    '    <td>'+SecondCityInfo.name+'</td>'+
+    '    <td>'+SecondCityInfo.name+'</td>'+
+    '    <td>'+SecondCityInfo.name+'</td>'+
+    '  </tr>'+
+    '</table>';
+  }
+  
+}
+
+function convertTime(time){
+
+
+return "d";
 }
